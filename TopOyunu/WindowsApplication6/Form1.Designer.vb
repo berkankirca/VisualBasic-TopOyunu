244425fef3d883e4259f5bc33039cf68dc1c6bc8
Berkan Kırca <berkankirca@hotmail.com>
2021-03-16 22:31:43 +0300

    Top Oyunu

---
+++ TopOyunu/WindowsApplication6/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.GroupBox1.Controls.Add(Me.PictureBox2)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(1, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(723, 417)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.PictureBox2.Location = New System.Drawing.Point(257, 398)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(209, 10)
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.PictureBox1.Image = Global.WindowsApplication6.My.Resources.Resources.Basket_Topu2
+        Me.PictureBox1.Location = New System.Drawing.Point(335, 40)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(54, 45)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(748, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(172, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(748, 41)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(172, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(781, 126)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(826, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Label2"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Timer2
+        '
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(730, 194)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(219, 160)
+        Me.ListBox1.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(950, 429)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ TopOyunu/WindowsApplication6/Form1.vb
@@ -0,0 +1,175 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Dim kontrol As Boolean 'cismin sağa veya sola gitmesini sağlayan kontrol
+    Dim kontrol2 As Boolean 'cismin yukarı aşağı hareketini belirleyen kontrol
+    Dim puan As Integer = 0 'oyuna başlangıç puanı
+    Dim x As Single = 3 'oyunun başında x ekseninin hızı
+    Dim y As Single = 3 'oyunun başında y ekseninin hızı
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'kontrol cismin sağa veya sola gitmesini sağlayan kontrol
+        If kontrol = True Then
+
+            PictureBox1.Left += x
+        Else
+            PictureBox1.Left -= x
+        End If
+        '=============================================
+        'cisim sola çarparsa sağa doğru yansır==
+        If PictureBox1.Left <= Me.ClientRectangle.Left Then
+            kontrol = True
+        End If
+        'cisim sağa çarparsa sola doğru yansır==
+        If PictureBox1.Left + PictureBox1.Width >= GroupBox1.Width Then
+            kontrol = False
+        End If
+        '===============================================
+        'kontrol2 cismin yukarı aşağı hareketini belirleyen boolean==
+        If kontrol2 = True Then
+            PictureBox1.Top -= y
+        Else
+            PictureBox1.Top += y
+
+        End If
+        '================================================
+        'topun alt üst çarpma kontrolleri
+        If PictureBox1.Top <= Me.ClientRectangle.Top Then
+            kontrol2 = False
+        End If
+
+        'If PictureBox1.Top + PictureBox1.Height >= Me.ClientRectangle.Bottom Then
+        '    kontrol2 = True
+
+        'End If
+        If PictureBox1.Bounds.IntersectsWith(PictureBox2.Bounds) Then
+            kontrol2 = True
+        End If
+        '======
+        'top yere düştüğünde verilen uyarı===
+        If PictureBox1.Bottom >= Me.ClientRectangle.Bottom Then
+            puan = 0
+            Dim cevap As String
+            Dim cevap2 As String
+            Timer1.Enabled = False
+            Timer2.Enabled = False
+            x = 3
+            y = 3
+            'listboxa puanı ekleme ve yeniden oynama sorusu====
+            cevap = InputBox("Oyun Bitti!" + " --- " + "Skorunuz:" + " " + CStr(Label2.Text) + "   " + "İsminiz:", "Oyun Bitti!", "İsimsiz")
+            ListBox1.Items.Add(cevap + ":  " + CStr(Label2.Text) + "  " + "/" & Now)
+            cevap2 = MsgBox("Tekrar Oynamak İster Misiniz?", vbYesNo, "Oyun")
+            If cevap2 = vbYes Then
+                'oyunu başlat butonu top, puan,hızlandırma sistemi devreye girer====
+                Timer1.Enabled = True
+                Timer2.Enabled = True
+                Me.KeyPreview = True
+                PictureBox2.Focus()
+                PictureBox1.Location = New Point(Rnd() * 100, Rnd() * 100)
+                PictureBox2.Location = New Point(257, 398)
+            Else
+                Me.Close()
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'timer özellikleri==
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+        Timer1.Interval = 10
+        Timer2.Interval = 1000
+        '====================
+        'buton ve label üzerinde yazacak metinler====
+        Button2.Text = "Oyunu Başlat"
+        Button3.Text = "Oyunu Bitir"
+        Label1.Text = "Skor:"
+        Label2.Text = "0"
+        '=====================================================================
+        Me.KeyPreview = False 'oyun başlamadan kontrolleri devre dışı bırakma
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'oyunu başlat butonu top, puan,hızlandırma sistemi devreye girer====
+        Timer1.Enabled = True
+        Timer2.Enabled = True
+        Me.KeyPreview = True
+        PictureBox2.Focus()
+        PictureBox1.Location = New Point(Rnd() * 100, Rnd() * 100)
+        PictureBox2.Location = New Point(257, 398)
+
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'oyunu bitir butonu top, puan,hızlandırma sistemi devre dışı kalır===
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+        Me.KeyPreview = False
+        puan = 0
+        x = 3
+        y = 3
+        'listboxa puanı ekleme ve yeniden oynama sorusu====
+        Dim cevap As String
+        Dim cevap2 As String
+        cevap = InputBox("Oyun Bitti!" + " --- " + "Skorunuz:" + " " + CStr(Label2.Text) + "   " + "İsminiz:", "Oyun Bitti!", "İsimsiz")
+        ListBox1.Items.Add(cevap + ":  " + CStr(Label2.Text) + "  " + "/" & Now)
+        cevap2 = MsgBox("Tekrar Oynamak İster Misiniz?", vbYesNo, "Oyun")
+        If cevap2 = vbYes Then
+            'oyunu başlat butonu top, puan,hızlandırma sistemi devreye girer====
+            Timer1.Enabled = True
+            Timer2.Enabled = True
+            Me.KeyPreview = True
+            PictureBox2.Focus()
+            PictureBox1.Location = New Point(Rnd() * 100, Rnd() * 100)
+            PictureBox2.Location = New Point(257, 398)
+        Else
+            Me.Close()
+        End If
+
+
+    End Sub
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        'saniyede 2 puan yükselten timer===
+        puan = puan + 2
+        Label2.Text = puan
+        'topun 10 puanda bir hızlanmasını sağlayan timer====
+        x = x + 0.1
+        y = y + 0.1
+    End Sub
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        'topu sektircek cismin oyundan çıkmadan hareketini sağlamak====
+        If e.KeyCode = Keys.Left Then
+            If PictureBox2.Left <= GroupBox1.Left Then
+            Else PictureBox2.Left -= 20
+            End If
+        End If
+        If e.KeyCode = Keys.Right Then
+            If PictureBox2.Left + PictureBox2.Width >= GroupBox1.Width Then
+            Else
+                PictureBox2.Left += 20
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        'formu kapatma isteğinde sorulacak soru
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+        puan = 0
+        Dim cevap3 As String
+        cevap3 = MsgBox("Oyundan Çıkmak İstediğinize Emin Misiniz?", vbYesNo, "Oyun")
+        If cevap3 = vbYes Then
+
+        Else
+            e.Cancel = True
+
+        End If
+    End Sub
+End Class
+
